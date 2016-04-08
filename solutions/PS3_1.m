6764744ccad7d24b6bb98d94e9f087ef71932a1d
LotannaEzenwa <lotanna.m.ezenwa.17@dartmouth.edu>
2016-04-08 13:09:14 -0400

    Added Shells for PS3 M Code
    LEGGGGOOO

---
+++ solutions/PS3_1.m
@@ -0,0 +1,12 @@
+%Lotanna Ezenwa, Problem Set 3, #1
+%PS3_1.m
+%% Due Wednesday, April 13th, 2016
+
+
+clear
+me = LotaEzenwa();
+code = me.id;
+PS_3_1
+
+Kp = 6800; %Newtons/Meter
+
---
+++ solutions/PS3_3.m
@@ -0,0 +1,18 @@
+%Lotanna Ezenwa, Problem Set 3, #3
+%PS3_3.m
+%% Due Wednesday, April 13th, 2016
+
+
+clear
+me = LotaEzenwa();
+code = me.id;
+PS_3_3
+
+
+%REMOVE TO SEE GRAPH
+close
+
+m = -5; %degC/meter
+b = -7; %degC
+L = 1; %Meter
+
