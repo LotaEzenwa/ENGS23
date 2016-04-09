1fa7e1389067763fb579b31ae4db4a40df78455d
LotannaEzenwa <lotanna.m.ezenwa.17@dartmouth.edu>
2016-04-09 10:42:11 -0400

    Found Eigenfrequencies

--- solutions/PS3_1.m
+++ solutions/PS3_1.m
@@ -9,4 +9,16 @@ code = me.id;
 PS_3_1
 
 Kp = 6800; %Newtons/Meter
+M = 10; %Kilograms
+K = 1000; %Newtons/Meter
+
+a1 = [-(K+Kp)/M, -Kp/M];
+a2 = [-(Kp/M),-(K+Kp)/M];
+
+A = [a1;a2];
+
+d = eig(A)';
+e1 = sqrt(-d(1));
+e2 = sqrt(-d(2));
+
 
