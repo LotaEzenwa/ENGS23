614e48d090d8e0d6abdb007a4c9f0098d40b1eb2
LotannaEzenwa <lotanna.m.ezenwa.17@dartmouth.edu>
2016-04-12 15:21:20 -0400

    updated solution to problem 1 with plot

--- solutions/PS3_1.m
+++ solutions/PS3_1.m
@@ -18,7 +18,7 @@ a2 = [-(Kp/M),-(K+Kp)/M]/(K/M);
 
 A = [a1;a2];
 
-dt = .1;
+dt = .001;
 d = eig(A)';
 e1 = sqrt(-d(1));
 e2 = sqrt(-d(2));
@@ -26,13 +26,26 @@ t = 0:dt:1;
 
 V1 = zeros(size(t));
 V2 = zeros(size(t));
+V1(1) = 1;
+V2(1) = -1;
+V1(2) = V1(1);
+V2(2) = V2(1);
 V = [V1;V2];
-
 for n = 3:length(V1)
-    DV1 = w0*(A(1,1)*V(1,n-1) + A(1,2)*V(2,n-1))*dt^2;
-    DV2 = w0*(A(2,1)*V(1,n-1) + A(2,2)*V(2,n-1))*dt^2;
+    DV1 = w0^2*(A(1,1)*V1(n-1) + A(1,2)*V2(n-1))*dt^2;
+    DV2 = w0^2*(A(2,1)*V1(n-1) + A(2,2)*V2(n-1))*dt^2;
     V1(n) = 2*V1(n-1) + DV1 - V1(n-2);
-    V2(n) = 2*V2(n-1) + DV2 - V1(n-2);
+    V2(n) = 2*V2(n-1) + DV2 - V2(n-2);
 end
 
+title_block = sprintf('%s - Problem Set 3, #1d',me.name);
+clf
+hold on
+grid on
 
+title(title_block);
+plot(t,V1);
+plot(t,V2);
+xlabel('t, sec');
+ylabel('Displacement, m');
+legend('X1(t)','X2(t)');
