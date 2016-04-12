2d0ebdd826777a14803d9b155c4f73c1febada09
LotannaEzenwa <lotanna.m.ezenwa.17@dartmouth.edu>
2016-04-12 10:23:44 -0400

    Got Homework scores/PS3_1.m
    And updated PS3_1

---
+++ solutions/Homework_Check.m
@@ -0,0 +1,9 @@
+%Lotanna Ezenwa, Homework Check
+%Homework_Check.m
+%% From April 12, 2016
+
+
+clear
+me = LotaEzenwa();
+code = me.id;
+HWscores
--- solutions/PS3_1.m
+++ solutions/PS3_1.m
@@ -12,13 +12,27 @@ Kp = 6800; %Newtons/Meter
 M = 10; %Kilograms
 K = 1000; %Newtons/Meter
 
-a1 = [-(K+Kp)/M, -Kp/M];
-a2 = [-(Kp/M),-(K+Kp)/M];
+w0 = sqrt(K/M);
+a1 = [-(K+Kp)/M, -Kp/M]/(K/M);
+a2 = [-(Kp/M),-(K+Kp)/M]/(K/M);
 
 A = [a1;a2];
 
+dt = .1;
 d = eig(A)';
 e1 = sqrt(-d(1));
 e2 = sqrt(-d(2));
+t = 0:dt:1;
+
+V1 = zeros(size(t));
+V2 = zeros(size(t));
+V = [V1;V2];
+
+for n = 3:length(V1)
+    DV1 = w0*(A(1,1)*V(1,n-1) + A(1,2)*V(2,n-1))*dt^2;
+    DV2 = w0*(A(2,1)*V(1,n-1) + A(2,2)*V(2,n-1))*dt^2;
+    V1(n) = 2*V1(n-1) + DV1 - V1(n-2);
+    V2(n) = 2*V2(n-1) + DV2 - V1(n-2);
+end
 
 
