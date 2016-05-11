8b6b0d30257f72a968e1096eb90388b7b61a4184
LotannaEzenwa <lotanna.m.ezenwa.17@dartmouth.edu>
2016-05-11 01:47:42 -0400

    Problem Shells for Problems 1-3

---
+++ solutions/PS7_1.m
@@ -0,0 +1,9 @@
+%Lotanna Ezenwa, Problem Set 7, #1
+%PS7_1.m
+%% Due Wednesday, May 11, 2016
+
+
+clear
+me = LotaEzenwa();
+code = me.id; 
+PS_7_1
---
+++ solutions/PS7_3.m
@@ -0,0 +1,9 @@
+%Lotanna Ezenwa, Problem Set 7, #3
+%PS7_3.m
+%% Due Wednesday, May 11, 2016
+
+
+clear
+me = LotaEzenwa();
+code = me.id; 
+PS_7_3
