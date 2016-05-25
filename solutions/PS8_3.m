c0968d20e742419b1b2fafc80997cf4314819892
LotannaEzenwa <lotanna.m.ezenwa.17@dartmouth.edu>
2016-05-25 11:42:37 -0400

    Finished solutions for PS8_3

--- solutions/PS8_3.m
+++ solutions/PS8_3.m
@@ -11,53 +11,45 @@ clf
 
 PS_4_3
 
-p = 120;%sec
-k = 125; %W/(m degC)
-rho = 9000; %kg/m^3
-cp = 500; %J/(kg degC)
+L = .22;
+tmax = 1000;
+D = 4.56*10^-5;
 
-T0 = 10; %degC
-L = .22; %meters
-dx = .005; %meters
-M = L/dx; %number of lumps
-D = k/(rho*cp); %Diff constant
-gamma = .05; 
-dt = dx^2*gamma/D;
-Tmax = 1000; %seconds
-t = 0:dt:Tmax;
-T_0 = sin(t*2*pi/p)*T0;
+Nx = 19;
+Nt = 1001;
 
-T = zeros(M,length(t));
-dTdt = zeros(M,1);
+Tinit = zeros(Nx,1);
+T0 = 10;
+TL = 0;
 
-N=M;
-A = diag([-3, -2*ones(1,N-2), -3]);
-A = A + diag(ones(1,N-1),1);
-A = A + diag(ones(1,N-1),-1);
+dt = tmax/(Nt-1);
+dx = L/(Nx+1);
 
-A = gamma*A;
-A = A + diag(ones(1,N));
+tspan = 0:dt:tmax;
+xspan = 0:dx:L;
 
+gamma = D*dt/dx^2;
+if gamma >= 0.5,
+    warning('Gamma off')
+end;
 
-
-for n=2:length(t)
-    T(1,n) = T0;
-    
-    for m = 2:M-1
-        T(m,n) = A(m,m-1)*T(m-1,n-1) + A(m,m)*T(m,n-1) + A(m,m+1)*T(m+1,n-1);
-    end
-    
-    T(M,n) = A(M,M-1)*T(M-1,n-1) + A(M,M)*T(M,n-1);
+if Nx == 1
+    error('Stop')
+else
+    A = (1-2*gamma)*diag(ones(1,Nx)) + gamma*diag(ones(1,Nx-1),1) + gamma*diag(ones(1,Nx-1),-1);
 end
 
-plot(t,(T(22,:) + T(23,:))/2)
-title_block = sprintf('%s - Problem Set 4, #3c, gamma = %d',me.name,gamma);
-hold on
-grid on
-title(title_block)
-ylabel('temperature, degC');
-xlabel('time, t');
-legend('x = 5cm');
+Bu = gamma*[T0;zeros(Nx-2,1);TL];
 
-t0 = find((T(22,:) + T(23,:))/2 > T0/4); %First point that x = 11cm is above T0/4;
-t0 = t0(1)*dx;
+
+T = zeros(Nx,Nt);
+T(:,1) = Tinit;
+for n=1:Nt-1
+    T(:,n+1) = A*T(:,n) + Bu;
+end
+T = [T0*ones(1,Nt);T;TL*ones(1,Nt)];
+
+plot(tspan,T(14,:))
+
+
+
